47cfa713651e998536f8388c686a0cd7ff11b448
Abdelrhman A. Elnhas <elnhasdesigner@gmial.com>
2026-03-31 18:55:09 +0200

    fix: Generalizing Breadcrumbs

--- src/components/Breadcrumbs/Breadcrumb.tsx
+++ src/components/Breadcrumbs/Breadcrumb.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 interface BreadcrumbProps {
   pageName: string;
+  parentCategoryName: string;
 }
 
-const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
+const Breadcrumb = ({ parentCategoryName, pageName }: BreadcrumbProps) => {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-[26px] font-bold leading-[30px] text-dark dark:text-white">
@@ -15,7 +16,7 @@ const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
         <ol className="flex items-center gap-2">
           <li>
             <Link className="font-medium" href="/">
-              DST /
+              {parentCategoryName} /
             </Link>
           </li>
           <li className="font-medium text-primary">{pageName}</li>
