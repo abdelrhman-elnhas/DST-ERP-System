97ca9c363e3aa62b2f3cbb902ef26e88c86f843b
Abdelrhman A. Elnhas <elnhasdesigner@gmial.com>
2026-03-31 18:56:57 +0200

    fix: query provider intialisation

--- src/app/providers/query-provider.tsx
+++ src/app/providers/query-provider.tsx
@@ -2,15 +2,17 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
+import { useState } from "react"
+
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient({
+  const [queryClient] = useState(() => new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: 1000 * 60 * 5,
         retry: 1,
       },
     },
-  });
+  }));
 
   return (
     <QueryClientProvider client={queryClient} >
