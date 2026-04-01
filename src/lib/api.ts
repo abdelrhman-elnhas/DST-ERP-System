a8ffee795717702f0023c4a4451a92af70591743
Abdelrhman A. Elnhas <elnhasdesigner@gmial.com>
2026-04-01 18:19:06 +0200

    fix: api to handle formData

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -13,10 +13,13 @@ function getTokenFromCookie(): string | null {
 export async function apiFetch(endpoint: string, options?: RequestInit) {
     const token = getTokenFromCookie();
 
+    const isFormData = options?.body instanceof FormData;
+
+
     const res = await fetch(`${BASE_URL}${endpoint}`, {
         ...options,
         headers: {
-            "Content-Type": "application/json",
+            ...(!isFormData && { "Content-Type": "application/json" }),
             ...(token && { Authorization: `Bearer ${token}` }),
             ...options?.headers,
         },
